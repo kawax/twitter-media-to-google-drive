d1d34223b6d0ca9db21049fd2771975df79798f3
kawax <kawaxbiz@gmail.com>
2017-04-10 17:31:24 +0900

    travis

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,8 @@
 git fetch origin
 git reset --hard origin/master
 /usr/local/bin/composer install --no-interaction --no-dev
-php artisan migrate --force
-php artisan route:cache
+#php artisan migrate --force
+#php artisan route:cache
 php artisan config:cache
 sudo chown -R apache:www ./
 sudo chmod 700 -R ./storage/
